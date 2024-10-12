75eca809a6c9491449f1fc9a2252bcf29ec7e695
Raynerchung888 <51701563+Raynerchung888@users.noreply.github.com>
2024-10-12 15:59:53 +0800

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,14 @@
+function main() {
+    window.open(`en/main.html` , `_self`);
+  }
+  function main_en() {
+    window.open(`main.html` , `_self`);
+  }
+  function main_tc() {
+    window.open(`main.html` , `_self`);
+  }
+  function main_sc() {
+    window.open(`main.html` , `_self`);
+  }
+
+  
